e4fce518db61143355a5388cae6c3c6413f2aeaa
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-14 12:43:42 +0400

    Added other models. They fail with cuda out of memory

--- eval.bash
+++ eval.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_gemma # Job name
+#SBATCH --job-name=cryptic_crosswords_mixtral_200g # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
 #SBATCH --ntasks=1                  # Run on a single CPU
-#SBATCH --mem=40G                   # Total RAM to be used
+#SBATCH --mem=200G                   # Total RAM to be used
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
@@ -14,6 +14,6 @@
 
 echo "starting Evaluation......................."
 
-python gemma_cryptic_crosswords_evaluate.py --run_name="replicating_paper_results1" &> ./logs/replicatings_gemma1.txt
+python mixtral_cryptic_crosswords_evaluate.py --run_name="mixtral_cryptic_crosswords_base" --prompt_name="base"
 
 echo " ending " 
