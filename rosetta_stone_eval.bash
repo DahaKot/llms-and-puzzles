6b27b9d77675271505f5494d0b3a73aa013bc0be
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-18 18:00:26 +0400

    Llama8b with rosetta sotne puzzles causes out of memory. Tried to launch on 2 gpus, but we'll see what happens

---
+++ rosetta_stone_eval.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=rosetta_stone_llama8b # Job name
+#SBATCH --error=logs/%j%x.err # error file
+#SBATCH --output=logs/%j%x.out # output log file
+#SBATCH --nodes=1                   # Run all processes on a single node    
+#SBATCH --ntasks=1                  # Run on a single CPU
+#SBATCH --mem=40G                   # Total RAM to be used
+#SBATCH --cpus-per-task=8          # Number of CPU cores
+#SBATCH -p cscc-gpu-p
+#SBATCH -q cscc-gpu-qos
+#SBATCH --gres=gpu:2                # Number of GPUs (per node)
+#SBATCH --time=12:00:00             # Specify the time needed for your experiment
+
+echo "starting Evaluation......................."
+
+python llama8b_rosetta_stone_evaluate.py --run_name="llama8b_rosetta_stone_base" --prompt_name="base"
+
+echo " ending " 
