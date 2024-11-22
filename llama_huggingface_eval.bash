67cf49ba31948069a4a77169ee8c5ee292e3fc3b
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-22 18:10:35 +0400

    trying to run llama with huggingface interface

--- llama_huggingface_eval.bash
+++ llama_huggingface_eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_mixtral_padding # Job name
+#SBATCH --job-name=cryptic_crosswords_llama_huggingface_batch_8 # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -9,11 +9,11 @@
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
-#SBATCH --gres=gpu:4                # Number of GPUs (per node)
+#SBATCH --gres=gpu:1                # Number of GPUs (per node)
 #SBATCH --time=12:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
-python llama8b_cryptic_crosswords_evaluate.py --run_name="llama_cryptic_crosswords_padding" --prompt_name="base" --batch_size=8
+python llama_huggingface_cryptic_crosswords_evaluate.py --run_name="llama_huggingface_cryptic_crosswords_batch_decode_batch8" --prompt_name="base" --batch_size=8
 
 echo " ending " 
