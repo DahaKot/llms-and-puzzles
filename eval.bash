7b3fee2f809d8b6eeae74c07b67e2d8306bb0846
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-25 15:22:18 +0400

    Fixed the dataset name bug

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_mixtral_instruct_base # Job name
+#SBATCH --job-name=cryptic_crosswords_llama8b_base # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -9,15 +9,13 @@
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
-#SBATCH --gres=gpu:4                # Number of GPUs (per node)
+#SBATCH --gres=gpu:1                # Number of GPUs (per node)
 #SBATCH --time=12:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
-# python mixtral_cryptic_crosswords_evaluate_vllm.py --run_name="mixtral_cryptic_crosswords_base_mixtral_instruct" --prompt_name="base_mixtral_instruct" --batch_size=8
-
-python inference.py --run_name="llama8b_cryptic_crosswords_base" \
-    --batch_size=8 --dataset="cryptic_crosswords" --model="llama8b" \
-    --prompt_name="base"
+python inference.py --run_name="cryptic_crosswords_base_llama8b" \
+    --batch_size=4 --dataset="cryptic_crosswords" --model="llama8b" \
+    --prompt_name="base" --n_gpus=1
 
 echo " ending " 
