8a4dbe562c1e395eba40a73bd45270ae410a71af
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-25 12:45:31 +0400

    changed the prompt for mixtral instruct

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_llama70b # Job name
+#SBATCH --job-name=cryptic_crosswords_mixtral_instruct_base # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -9,11 +9,11 @@
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
-#SBATCH --gres=gpu:2                # Number of GPUs (per node)
+#SBATCH --gres=gpu:4                # Number of GPUs (per node)
 #SBATCH --time=12:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
-python llama70b_cryptic_crosswords_evaluate.py --run_name="llama70b_cryptic_crosswords_base_2gpus" --prompt_name="base" --batch_size=8
+python mixtral_cryptic_crosswords_evaluate_vllm.py --run_name="mixtral_cryptic_crosswords_base_mixtral_instruct" --prompt_name="base_mixtral_instruct" --batch_size=8
 
 echo " ending " 
