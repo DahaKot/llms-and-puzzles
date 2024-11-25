e3dde8c165017b9b91a18f9b27798d1b3c1e6e10
Daria Kotova <dariakotova42@gmail.com>
2024-11-25 15:20:01 +0400

    Pep8 the code

--- models_list.py
+++ models_list.py
@@ -1,2 +1,2 @@
 models_dict = {"llama8b": "meta-llama/Llama-3.1-8B-Instruct",
-          "mixtral7x8b": "mistralai/Mixtral-8x7B-Instruct-v0.1"}
+               "mixtral7x8b": "mistralai/Mixtral-8x7B-Instruct-v0.1"}
