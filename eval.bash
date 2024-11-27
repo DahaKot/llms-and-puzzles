0b1e1f3d2bf744ccb6978c8504aa77e8d1462f0a
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-11-27 17:39:30 +0400

    Added more models (llama8b, mixtral7x8b, qwen14b is the final list?)

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_llama8b_base # Job name
+#SBATCH --job-name=rosetta_stone_llama8b_base_batch256 # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -10,12 +10,14 @@
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
 #SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH --time=12:00:00             # Specify the time needed for your experiment
+#SBATCH --time=00:30:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
-python inference.py --run_name="cryptic_crosswords_base_llama8b" \
-    --batch_size=4 --dataset="cryptic_crosswords" --model="llama8b" \
+nvidia-smi
+
+python inference.py --run_name="rosetta_stone_base_llama8b_batch32" \
+    --batch_size=32 --dataset="rosetta_stone" --model="llama8b" \
     --prompt_name="base" --n_gpus=1
 
 echo " ending " 
