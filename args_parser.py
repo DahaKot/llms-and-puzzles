61f0b468bb2425760e2192f7e73a7fccdd0d7b63
Daria Kotova <dariakotova42@gmail.com>
2024-11-27 18:06:59 +0400

    Added logic puzzles dataset. Not sure about parsing the correct answer yet

--- args_parser.py
+++ args_parser.py
@@ -14,10 +14,12 @@ def add_args(parser: argparse.ArgumentParser):
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--n_gpus", type=int, default=1)
     parser.add_argument(
-        "--dataset", type=str, choices=["cryptic_crosswords", "rosetta_stone"]
+        "--dataset", type=str, choices=["cryptic_crosswords", "rosetta_stone",
+                                        "logic"]
     )
     parser.add_argument(
-        "--model", type=str, choices=["llama8b", "mixtral7x8b", "falcon40b", "mistral7b", "qwen"]
+        "--model", type=str, choices=["llama8b", "mixtral7x8b", "falcon40b",
+                                      "mistral7b", "qwen"]
     )
     parser.add_argument(
         "--prompt_name", type=str, default="base", choices=["base", "advanced"]
