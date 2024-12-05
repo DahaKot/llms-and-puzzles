23661f31e42e233aaeedfca6286aae5c7e25319a
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2024-12-05 12:02:05 +0400

    Launched experiments on full rosetta stone dataset. without special prompt for mixtral

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=logic_puzzles_llama_base_batch16 # Job name
+#SBATCH --job-name=rosetta_stone_mixtral_base_batch16_full_dataset # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -9,15 +9,15 @@
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
-#SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH --time=01:00:00             # Specify the time needed for your experiment
+#SBATCH --gres=gpu:4                # Number of GPUs (per node)
+#SBATCH --time=02:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
 nvidia-smi
 
-python inference.py --run_name="logic_puzzles_base_llama8b_changed_dataset_changes_accuracy?" \
-    --batch_size=16 --dataset="logic_puzzles" --model="llama8b" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=1
+python inference.py --run_name="rosetta_stone_base_mixtral_full_dataset" \
+    --batch_size=16 --dataset="rosetta_stone" --model="mixtral7x8b" \
+    --prompt_name="base" --n_gpus=4
 
 echo " ending " 
