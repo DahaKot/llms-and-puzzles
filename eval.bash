64da5f9ba7226afbf676c40f9728eb959e6d72a7
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-01-07 12:18:23 +0400

    Debugging logic puzzles answer extarction

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=rosetta_stone # Job name
+#SBATCH --job-name=logic_puzzles_llama_advanced # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -16,32 +16,32 @@ echo "starting Evaluation......................."
 
 nvidia-smi
 
-dataset_name="rosetta_stone"
-batch_size=8
-max_tokens=512
+dataset_name="logic_puzzles"
+batch_size=256
+max_tokens=256
 
 echo "llama base"
 
-python inference.py --run_name="updated_prompt_rosetta_stone_llama_base_max_tokens_512" \
+python inference.py --run_name="updated_prompt_logic_puzzles_llama_base" \
     --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
+    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens --log_probs=512
 
 echo "llama advanced"
 
-python inference.py --run_name="updated_prompt_rosetta_stone_llama_advanced_max_tokens_512" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="updated_prompt_rosetta_stone_llama_advanced_max_tokens_512_inter_prompt" \
+#    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
+#    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
 
 echo "qwen base"
 
-python inference.py --run_name="updated_prompt_rosetta_stone_qwen_base_max_tokens_512" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="updated_prompt_rosetta_stone_qwen_base_max_tokens_512_inter_prompt" \
+#    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
+#    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
 
 echo "qwen advanced"
 
-python inference.py --run_name="updated_prompt_rosetta_stone_qwen_advanced_max_tokens_512" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="updated_prompt_rosetta_stone_qwen_advanced_max_tokens_512_inter_prompt" \
+#    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
+#    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
 
 echo " ending " 
