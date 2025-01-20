1b80fb8424c971168e8d5567fff8be302d919046
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-01-20 15:15:17 +0400

    Run mixtral experiments. waiting in the queue

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=logic_puzzles_llama_advanced # Job name
+#SBATCH --job-name=logic_puzzles_llama_base # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -17,20 +17,20 @@ echo "starting Evaluation......................."
 nvidia-smi
 
 dataset_name="logic_puzzles"
-batch_size=256
+batch_size=16
 max_tokens=256
 
-echo "llama base"
+echo "logic puzzles base llama8b"
 
-python inference.py --run_name="updated_prompt_logic_puzzles_llama_base" \
+python inference.py --run_name="logic_puzzles_llama_base" \
     --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens --log_probs=512
+    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
 
-echo "llama advanced"
+echo "cryptic crosswords mixtral cot"
 
-#python inference.py --run_name="updated_prompt_rosetta_stone_llama_advanced_max_tokens_512_inter_prompt" \
-#    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-#    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="cryptic_crosswords_mixtral_zero_shot_chain_of_thought" \
+#    --batch_size=256 --dataset="cryptic_crosswords" --model="mixtral7x8b" \
+#    --prompt_name="zero_shot_chain_of_thought_mixtral_instruct" --n_gpus=4 --max_tokens=256
 
 echo "qwen base"
 
