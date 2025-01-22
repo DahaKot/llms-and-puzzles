b7de0f2af772e691ad24ba06b8085482ba63cd6d
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-01-22 15:06:32 +0400

    Removed two problems from puzzleben with strange answer options.

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=logic_puzzles_llama_base # Job name
+#SBATCH --job-name=logic_puzzles_qwen_advanced # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -10,7 +10,7 @@
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
 #SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH --time=03:00:00             # Specify the time needed for your experiment
+#SBATCH --time=00:40:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
@@ -22,9 +22,9 @@ max_tokens=256
 
 echo "logic puzzles base llama8b"
 
-python inference.py --run_name="logic_puzzles_llama_base" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
+python inference.py --run_name="logic_puzzles_qwen_advanced_answer_format" \
+    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
+    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
 
 echo "cryptic crosswords mixtral cot"
 
