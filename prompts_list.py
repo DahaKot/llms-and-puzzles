bfabab5a2955238db0a0faa8b4822c37721cbe96
Daria Kotova <dariakotova42@gmail.com>
2025-02-05 20:37:31 +0400

    Added 5 shot prompts

--- prompts_list.py
+++ prompts_list.py
@@ -15,6 +15,37 @@ Extract the definition and the wordplay from the clue, then use them to solve it
 Clue:
 {clue}
 ''',
+"5_shot": '''You are a cryptic crossword expert. You are given a cryptic crossword clue. Solve the clue and provide only the answer.
+Examples:
+Clue:
+{clue1}
+Answer:
+{answer1}
+
+Clue:
+{clue2}
+Answer:
+{answer2}
+
+Clue:
+{clue3}
+Answer:
+{answer3}
+
+Clue:
+{clue4}
+Answer:
+{answer4}
+
+Clue:
+{clue5}
+Answer:
+{answer5}
+
+Clue:
+{clue}
+Answer:
+''',
 "base_mixtral_instruct": '''[INST] You are a cryptic crossword expert. You are given a cryptic crossword clue. Solve the clue and provide only the answer.
 Clue:
 {clue}
@@ -32,7 +63,39 @@ A cryptic clue typically consists of a definition and a wordplay. The definition
 Extract the definition and the wordplay from the clue, then use them to solve it. Finally, provide the answer in the format "Answer: ...".
 Clue:
 {clue}
-[/INST]'''
+[/INST]''',
+"5_shot_mixtral_instruct": '''[INST] You are a cryptic crossword expert. You are given a cryptic crossword clue. Solve the clue and provide only the answer.
+Examples:
+Clue:
+{clue1}
+Answer:
+{answer1}
+
+Clue:
+{clue2}
+Answer:
+{answer2}
+
+Clue:
+{clue3}
+Answer:
+{answer3}
+
+Clue:
+{clue4}
+Answer:
+{answer4}
+
+Clue:
+{clue5}
+Answer:
+{answer5}
+
+Clue:
+{clue}
+Answer:
+[/INST]
+'''
 }
 
 rosetta_stone_prompts = {"base": '''You are a linguistic expert. You are given a Rosetta Stone puzzle containing several parallel sentences in two languages, followed by a new sentence to translate. Translate the sentence and provide only the answer.
@@ -55,6 +118,49 @@ Parallel sentences:
 <<DATA>>
 Analyze how the language works and apply the deduced rules to translate the new sentence. Finally, provide the answer in the format "Answer: ...".
 <<QUESTION>>
+''',
+"5_shot": '''You are a linguistic expert. You are given a Rosetta Stone puzzle containing several parallel sentences in two languages, followed by a new sentence to translate. Translate the sentence and provide only the answer.
+Examples:
+Parallel sentences:
+<<DATA1>>
+Sentence to translate:
+<<QUESTION1>>
+Answer:
+<<ANSWER1>>
+
+Parallel sentences:
+<<DATA2>>
+Sentence to translate:
+<<QUESTION2>>
+Answer:
+<<ANSWER2>>
+
+Parallel sentences:
+<<DATA3>>
+Sentence to translate:
+<<QUESTION3>>
+Answer:
+<<ANSWER3>>
+
+Parallel sentences:
+<<DATA4>>
+Sentence to translate:
+<<QUESTION4>>
+Answer:
+<<ANSWER4>>
+
+Parallel sentences:
+<<DATA5>>
+Sentence to translate:
+<<QUESTION5>>
+Answer:
+<<ANSWER5>>
+
+Parallel sentences:
+<<DATA>>
+Sentence to translate:
+<<QUESTION>>
+Answer:
 ''',
 "base_mixtral_instruct": '''[INST] You are a linguistic expert. You are given a Rosetta Stone puzzle containing several parallel sentences in two languages, followed by a new sentence to translate.
 Parallel sentences:
@@ -79,7 +185,52 @@ Parallel sentences:
 Analyze how the language works and apply the deduced rules to translate the new sentence. Finally, provide the answer in the format "Answer: ...".
 <<QUESTION>>
 [/INST]
-'''}
+''',
+"5_shot_mixtral_instruct": '''[INST] You are a linguistic expert. You are given a Rosetta Stone puzzle containing several parallel sentences in two languages, followed by a new sentence to translate. Translate the sentence and provide only the answer.
+Examples:
+Parallel sentences:
+<<DATA1>>
+Sentence to translate:
+<<QUESTION1>>
+Answer:
+<<ANSWER1>>
+
+Parallel sentences:
+<<DATA2>>
+Sentence to translate:
+<<QUESTION2>>
+Answer:
+<<ANSWER2>>
+
+Parallel sentences:
+<<DATA3>>
+Sentence to translate:
+<<QUESTION3>>
+Answer:
+<<ANSWER3>>
+
+Parallel sentences:
+<<DATA4>>
+Sentence to translate:
+<<QUESTION4>>
+Answer:
+<<ANSWER4>>
+
+Parallel sentences:
+<<DATA5>>
+Sentence to translate:
+<<QUESTION5>>
+Answer:
+<<ANSWER5>>
+
+Parallel sentences:
+<<DATA>>
+Sentence to translate:
+<<QUESTION>>
+Answer:
+[/INST]
+''',
+}
 
 logic_puzzles_prompts = {"base": '''You are a logic expert. You are given a logic puzzle along with several answer options. Solve the puzzle and provide only the letter of the correct option in the format: "answer: <option letter>".
 Puzzle:
@@ -103,6 +254,49 @@ Puzzle:
 Possible answers:
 {options}
 ''',
+"5_shot": '''You are a logic expert. You are given a logic puzzle along with several answer options. Solve the puzzle and provide only the letter of the correct option in the format: "answer: <option letter>".
+Examples:
+Puzzle:
+{problem1}
+Possible answers:
+{options1}
+Answer:
+{answer1}
+
+Puzzle:
+{problem2}
+Possible answers:
+{options2}
+Answer:
+{answer2}
+
+Puzzle:
+{problem3}
+Possible answers:
+{options3}
+Answer:
+{answer3}
+
+Puzzle:
+{problem4}
+Possible answers:
+{options4}
+Answer:
+{answer4}
+
+Puzzle:
+{problem5}
+Possible answers:
+{options5}
+Answer:
+{answer5}
+
+Puzzle:
+{problem}
+Possible answers:
+{options}
+Answer:
+''',
 "base_mixtral_instruct": '''[INST] You are a logic expert. You are given a logic puzzle along with several answer options. Solve the puzzle and provide only the number of the correct option.
 Puzzle:
 {problem}
@@ -116,5 +310,49 @@ Puzzle:
 {problem}
 Possible answers:
 {options}
-[/INST]'''
+[/INST]''',
+"5_shot_mixtral_instruct": '''[INST] You are a logic expert. You are given a logic puzzle along with several answer options. Solve the puzzle and provide only the letter of the correct option in the format: "answer: <option letter>".
+Examples:
+Puzzle:
+{problem1}
+Possible answers:
+{options1}
+Answer:
+{answer1}
+
+Puzzle:
+{problem2}
+Possible answers:
+{options2}
+Answer:
+{answer2}
+
+Puzzle:
+{problem3}
+Possible answers:
+{options3}
+Answer:
+{answer3}
+
+Puzzle:
+{problem4}
+Possible answers:
+{options4}
+Answer:
+{answer4}
+
+Puzzle:
+{problem5}
+Possible answers:
+{options5}
+Answer:
+{answer5}
+
+Puzzle:
+{problem}
+Possible answers:
+{options}
+Answer:
+[/INST]
+''',
 }
