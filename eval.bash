2ae501c2c5c88e33ebb2557f30032ae30d590319
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-02-06 13:49:43 +0400

    Added 5_shot option to args parser. Running cryptic crosswords with random shots

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=logic_puzzles_qwen_512_tokens # Job name
+#SBATCH --job-name=cryptic_crosswords_5_random_shots # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -16,27 +16,27 @@ echo "starting Evaluation......................."
 
 nvidia-smi
 
-dataset_name="logic_puzzles"
-batch_size=64
-max_tokens=512
+dataset_name="cryptic_crosswords"
+batch_size=256
+max_tokens=256
 
 echo "logic_puzzles base mixtral"
 
-python inference.py --run_name="logic_puzzles_qwen_base" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="base" --n_gpus=1 --max_tokens=$max_tokens
+python inference.py --run_name="cryptic_crosswords_llama_random_shots" \
+    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
+    --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
 
 echo "logic puzzles advanced mixtral"
 
-python inference.py --run_name="logic_puzzles_qwen_advanced" \
+python inference.py --run_name="cryptic_crosswords_qwen_random_shots" \
     --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
+    --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
 
 echo "logic puzzles zero shot chain of thought mixtral"
 
-python inference.py --run_name="logic_puzzles_qwen_zero_shot_chain_of_thought" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="zero_shot_chain_of_thought" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="logic_puzzles_qwen_zero_shot_chain_of_thought" \
+#    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
+#    --prompt_name="zero_shot_chain_of_thought" --n_gpus=1 --max_tokens=$max_tokens
 
 #echo "logic puzzles advanced qwen"
 
