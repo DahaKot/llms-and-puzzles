bb736abc6dc5f83439b7e0f49e8b2b0998fe657e
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-02-12 17:43:05 +0400

    Running experiments

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_5_random_shots # Job name
+#SBATCH --job-name=cryptic_crosswords_mixtral # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -9,8 +9,8 @@
 #SBATCH --cpus-per-task=8          # Number of CPU cores
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
-#SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH --time=03:00:00             # Specify the time needed for your experiment
+#SBATCH --gres=gpu:4               # Number of GPUs (per node)
+#SBATCH --time=04:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
@@ -19,24 +19,19 @@ nvidia-smi
 dataset_name="cryptic_crosswords"
 batch_size=256
 max_tokens=256
+model="mixtral7x8b"
 
-echo "logic_puzzles base mixtral"
+python inference.py --run_name="cryptic_crosswords_mixtral_base" \
+    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
+    --prompt_name="base_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens
 
-python inference.py --run_name="cryptic_crosswords_llama_random_shots" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="llama8b" \
-    --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
+python inference.py --run_name="cryptic_crosswords_mixtral_advanced" \
+    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
+    --prompt_name="advanced_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens
 
-echo "logic puzzles advanced mixtral"
-
-python inference.py --run_name="cryptic_crosswords_qwen_random_shots" \
-    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-    --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
-
-echo "logic puzzles zero shot chain of thought mixtral"
-
-#python inference.py --run_name="logic_puzzles_qwen_zero_shot_chain_of_thought" \
-#    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-#    --prompt_name="zero_shot_chain_of_thought" --n_gpus=1 --max_tokens=$max_tokens
+python inference.py --run_name="cryptic_crosswords_mixtral_zero_shot_chain_of_thought" \
+    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
+    --prompt_name="zero_shot_chain_of_thought_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens
 
 #echo "logic puzzles advanced qwen"
 
