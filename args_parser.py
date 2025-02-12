e5bfd2bbab43ea790456ea38821edfaa3e537861
Daria Kotova <dariakotova42@gmail.com>
2025-02-12 17:50:45 +0400

    Runnning random shots experiments

--- args_parser.py
+++ args_parser.py
@@ -13,6 +13,7 @@ def add_args(parser: argparse.ArgumentParser):
     parser.add_argument("--run_name", type=str, required=True)
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--n_gpus", type=int, default=1)
+    parser.add_argument("--n_shots", type=int, default=5)
     parser.add_argument("--max_tokens", type=int, default=256)
     parser.add_argument("--logprobs", type=int, default=1)
     parser.add_argument(
@@ -21,7 +22,7 @@ def add_args(parser: argparse.ArgumentParser):
     )
     parser.add_argument(
         "--model", type=str,
-        choices=["llama8b", "mixtral7x8b", "qwen", "deepseek"]
+        choices=["llama", "mixtral", "qwen", "deepseek"]
     )
     parser.add_argument(
         "--prompt_name", type=str, default="base",
@@ -36,5 +37,5 @@ def add_args(parser: argparse.ArgumentParser):
     )
     parser.add_argument(
         "--order", type=str, default="random",
-        choices=["random", "most_similar", "least_similar", "other_categories"]
+        choices=["random", "most_similar", "least_similar"]
     )
