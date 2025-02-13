cb8cc03636a30ed42bc4b2278342b3e50735fb1a
Daria Kotova <dariakotova42@gmail.com>
2025-02-13 12:47:59 +0400

    Fixed the namings of the models

--- models_list.py
+++ models_list.py
@@ -1,6 +1,3 @@
-models_dict = {"llama8b": "meta-llama/Llama-3.1-8B-Instruct",
-               "mixtral7x8b": "mistralai/Mixtral-8x7B-Instruct-v0.1",
-               "llama70b": "meta-llama/Llama-3.1-70B-Instruct",
-               "mistral7b": "mistralai/Mistral-7B-Instruct-v0.3",
-               "falcon40b": "tiiuae/falcon-40b-instruct",
+models_dict = {"llama": "meta-llama/Llama-3.1-8B-Instruct",
+               "mixtral": "mistralai/Mixtral-8x7B-Instruct-v0.1",
                "qwen": "Qwen/Qwen2.5-14B-Instruct"}
