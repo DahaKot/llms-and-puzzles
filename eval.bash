9dd4cda4b6add2959fbab616122454e1f4315060
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-02-13 12:46:39 +0400

    unning random shots experiments

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cryptic_crosswords_llama_random_shots # Job name
+#SBATCH --job-name=cryptic_crosswords_random_shots # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -10,7 +10,7 @@
 #SBATCH -p cscc-gpu-p
 #SBATCH -q cscc-gpu-qos
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
-#SBATCH --time=01:00:00             # Specify the time needed for your experiment
+#SBATCH --time=03:00:00             # Specify the time needed for your experiment
 
 echo "starting Evaluation......................."
 
@@ -21,24 +21,22 @@ batch_size=256
 max_tokens=256
 model="llama"
 
-python inference.py --run_name="cryptic_crosswords_llama_random_shots" \
+python inference.py --run_name="cryptic_crosswords_llama_random_shots2" \
     --batch_size=$batch_size --dataset=$dataset_name --model=$model \
     --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens \
     --similarity="random" --order="random"
 
-# python inference.py --run_name="cryptic_crosswords_mixtral_advanced" \
-#     --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-#     --prompt_name="advanced_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens
+python inference.py --run_name="cryptic_crosswords_qwen_random_shots2" \
+     --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
+     --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
 
-# python inference.py --run_name="cryptic_crosswords_mixtral_zero_shot_chain_of_thought" \
-#     --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-#     --prompt_name="zero_shot_chain_of_thought_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens
+#python inference.py --run_name="rosetta_stone_llama_random_shots" \
+#     --batch_size=8 --dataset="rosetta_stone" --model=$model \
+#     --prompt_name="5_shot" --n_gpus=1 --max_tokens=$max_tokens
 
-#echo "logic puzzles advanced qwen"
-
-#python inference.py --run_name="logic_puzzles_qwen_advanced" \
-#    --batch_size=$batch_size --dataset=$dataset_name --model="qwen" \
-#    --prompt_name="advanced" --n_gpus=1 --max_tokens=$max_tokens
+#python inference.py --run_name="rosetta_stone_qwen_random_shots" \
+#    --batch_size=8 --dataset="rosetta_stone" --model="qwen" \
+#    --prompt_name="5_shots" --n_gpus=1 --max_tokens=$max_tokens
 
 #echo "logic puzzles cot qwen"
 
