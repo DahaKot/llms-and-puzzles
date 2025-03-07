449f3685b340ab8e6551f5d3f039e548a4b0833c
Daria Kotova <daria.kotova@mbzuai.ac.ae>
2025-03-07 12:51:07 +0400

    Mixtral with solutions

--- eval.bash
+++ eval.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=rosetta_stone_mixtral_icl_test # Job name
+#SBATCH --job-name=rosetta_stone_mixtral_icl # Job name
 #SBATCH --error=logs/%j%x.err # error file
 #SBATCH --output=logs/%j%x.out # output log file
 #SBATCH --nodes=1                   # Run all processes on a single node    
@@ -16,71 +16,15 @@ echo "starting Evaluation......................."
 
 nvidia-smi
 
-dataset_name="rosetta_stone"
-batch_size=8
+dataset_name="cryptic_crosswords_types"
+batch_size=256
 max_tokens=512
 model="mixtral"
-similarity="random"
+similarity="thematic"
 
-python inference.py --run_name="rosetta_stone_mixtral_random1" \
+python inference.py --run_name="small_cryptic_crosswords_mixtral_solutions_thematic_bottom_to_top" \
     --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=3453
-
-python inference.py --run_name="rosetta_stone_mixtral_random2" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=128
-
-python inference.py --run_name="rosetta_stone_mixtral_random3" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=256
-
-python inference.py --run_name="rosetta_stone_mixtral_random4" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=512
-
-python inference.py --run_name="rosetta_stone_mixtral_random5" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=1024
-
-similarity="semantic"
-python inference.py --run_name="rosetta_stone_mixtral_semantic_random1" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=1024
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_random2" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=64
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_random3" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=32
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_random4" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=16
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_random5" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="random" --random_seed=8
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_top_to_bottom" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="semantic_top_to_bottom" --random_seed=1024
-
-python inference.py --run_name="rosetta_stone_mixtral_semantic_bottom_to_top" \
-    --batch_size=$batch_size --dataset=$dataset_name --model=$model \
-    --prompt_name="5_shot_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
-    --similarity=$similarity --ranking="semantic_bottom_to_top" --random_seed=1024
+    --prompt_name="5_shot_solutions_mixtral_instruct" --n_gpus=4 --max_tokens=$max_tokens \
+    --similarity=$similarity --ranking="semantic_bottom_to_top" --random_seed=3453
 
 echo " ending " 
